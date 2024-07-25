809494bf7c9ec65656c4eaa2a3c2041bde30087b
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-25 22:45:20 +0200

    [FEAT] Create Vue App

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const { createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            // email: null
+        }
+    },
+    methods: {
+        // generateEmail
+    }
+}).mount(`#app`);
