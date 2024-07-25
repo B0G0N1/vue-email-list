3519e14fee514e0294b0b5dc01d94690ebc1d216
B0G0N1 <michelebogoni2001@gmail.com>
2024-07-26 00:59:49 +0200

    [FEAT] Finish project and comment, FINAL PROJECT

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Creao APP  di Vue.js
 const { createApp } = Vue;
 
 createApp({
@@ -7,10 +8,17 @@ createApp({
         };
     },
     methods: {
-        generateEmail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+        // Metodo per generare lista di 10 Email
+        generateEmailList () {
+            // Pulisco ogni volta l'array
+            this.emailList = [];
+            // Itero 10 volte
+            for (let i = 0; i < 10; i++) {
+                // Recupero valore da API
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
                     this.emailList.push(resp.data.response);
                 });
+            }
         }
     }
 }).mount('#app');
